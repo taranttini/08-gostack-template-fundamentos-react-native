ac792bdd5433148d2a10de4a791af4aea94d6e0d
Tarantini Pereira <taranttini@hotmail.com>
2020-08-11 23:21:45 -0300

    resolvendo desafio

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: 'http://192.168.15.3:3333',
 });
 
 export default api;
+//  yarn json-server server.json -p 3333 --host 192.168.15.3
